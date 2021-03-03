1a67e5093d5f8c1f2e1592d622d7c4926b5e18a9
Conor Loftus <conor.lo.lynch556@gmail.com>
2021-03-03 14:13:26 +0000

    Added code to detect faces in a still image.

--- recognition.py
+++ recognition.py
@@ -5,12 +5,6 @@ Early Steps (Non anaconda approach)
 2) Make sure you pip install opencv-python (4.3.0.36 is the version I am using)
 3) Test that the pip install works for opencv (test importing it)
 
-Early Steps (Anaconda approach)
-1) Create Anaconda Virtual Environment: conda create --name opencv-env python=3.8
-2) Activate the environment: conda activate opencv-env
-3) Enter the command: pip freeze to see what python modules are installed
-4) Deactivate the environment by: conda deactivate
-
 Design Steps
 -------------------------------------------------------------------------------
 1) Now need to think about how we get the video into the python program
@@ -19,7 +13,39 @@ Design Steps
 
 """
 
-import cv2
+import cv2, sys
+
+# Get user supplied values
+imagePath = sys.argv[1]
+cascPath = "haarcascade_frontalface_default.xml"
+
+# Create the haar cascade
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+
+# Read the image
+image = cv2.imread(imagePath)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+
+# Detect faces in the image
+faces = faceCascade.detectMultiScale(
+    gray,
+    scaleFactor=1.1,
+    minNeighbors=5,
+    minSize=(30, 30)
+    #flags = cv2.CV_HAAR_SCALE_IMAGE
+)
+
+print("Found {0} faces!".format(len(faces)))
+
+# Draw a rectangle around the faces
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+cv2.imshow("Faces found", image)
+cv2.waitKey(0)
+
 
 # Here is the main function
 def main():
